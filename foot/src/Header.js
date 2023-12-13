76a787531931148b6e57e3f304bc0179f1394ff5
Sushant Singh <sushantbibhu@gmail.com>
2023-12-13 18:04:30 +0530

    Update CSS and add navigation bar to the header.

--- foot/src/Header.js
+++ foot/src/Header.js
@@ -5,7 +5,19 @@ import React from "react";
 function Header() {
   return (
     <>
-      <h1 className="h1">Header</h1>
+      <nav>
+        <a href="#home">Foot</a>
+        <a href="#home">Home</a>
+        <a href="#about">About</a>
+        <a href="#services">Services</a>
+        <a href="#contact">Contact</a>
+        <a className="login" href="#cart">
+          Cart
+        </a>
+        <a className="login" href="#login">
+          Login
+        </a>
+      </nav>
     </>
   );
 }
