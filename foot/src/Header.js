10abac2fe1b4e784fc34ad78621b32c04523755c
Sushant Singh <sushantbibhu@gmail.com>
2023-12-14 10:58:34 +0530

    Add Font Awesome icons and Heroicons to the project.

--- foot/src/Header.js
+++ foot/src/Header.js
@@ -1,16 +1,20 @@
 import "./Header.css";
-
 import React from "react";
+import Cart from "./icons/Cart";
 
 function Header() {
   return (
     <>
       <nav>
-        <a href="#home">Foot</a>
+        <a className="home" href="#home">
+          Foot
+        </a>
         <a href="#home">Home</a>
         <a href="#about">About</a>
         <a href="#services">Services</a>
         <a href="#contact">Contact</a>
+        <Cart />
+
         <a className="login" href="#cart">
           Cart
         </a>
