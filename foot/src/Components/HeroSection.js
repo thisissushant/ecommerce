5671de4fb03363642fbd46d0ecf4992fdc67ff1e
Sushant Singh <sushantbibhu@gmail.com>
2023-12-22 08:46:51 +0530

    Changed the background image and reduced the height of the hero section. Removed the hero overlay.

--- foot/src/Components/HeroSection.js
+++ foot/src/Components/HeroSection.js
@@ -3,8 +3,7 @@ import "./HeroSection.css";
 
 const HeroSection = () => {
   return (
-    <div className="hero-section  ">
-      <div className="hero-overlay"></div>
+    <div className="hero-section size-full ">
       <div className="hero-content">
         <h1 className="hero-heading">Best Sneakers</h1>
         <p className="hero-subheading">Buy Affordable and Best Sneakers</p>
