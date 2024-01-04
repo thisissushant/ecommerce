6f28cf9a3df7c05365da1255089d5939e1f5a78d
Sushant Singh <sushantbibhu@gmail.com>
2024-01-04 10:27:47 +0530

    Changed the title of the shop page from "Top Selling" to "Products" and added an "Add to Cart" button.

--- foot/src/Components/Shop.js
+++ foot/src/Components/Shop.js
@@ -5,9 +5,7 @@ export default function Shop() {
   return (
     <>
       <div className=" bg-gradient-to-r from-zinc-200 from-10% via-amber-100 via-30% to-zinc-400 to-90%">
-        <h2 className="grid justify-items-center py-4  text-4xl">
-          Top Selling
-        </h2>
+        <h2 className="grid justify-items-center py-4  text-4xl">Products</h2>
         <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-4 lg:max-w-7xl lg:px-8 py-4">
           <h2 className="sr-only">Products</h2>
 
@@ -21,10 +19,19 @@ export default function Shop() {
                     className="h-64 w-full rounded-lg object-cover object-center group-hover:opacity-75 "
                   />
                 </div>
+
                 <h3 className="mt-4 text-sm text-black">{product.name}</h3>
                 <p className="mt-1 text-lg font-medium text-black">
                   {product.price}
                 </p>
+                <div className="flex flex-col space-y-2 md:flex-row-reverse md:space-x-2 md:space-y-0 md:flex-col ">
+                  <button
+                    type="button"
+                    className="rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
+                  >
+                    Add to Cart
+                  </button>
+                </div>
               </a>
             ))}
           </div>
